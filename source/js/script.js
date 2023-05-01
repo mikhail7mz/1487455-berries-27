ba69e1533b832829e28563be0b147cb350f923d9
Mikhail <65595208+mikhail7mz@users.noreply.github.com>
2023-05-01 21:39:05 +0300

    add styles

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,13 @@
 /* в этот файл добавляет скрипты*/
+
+const menuToggle = document.querySelector('.header__menu-toggle');
+const menu = document.querySelector('.header__menu');
+
+document.querySelector('.header--no-js').classList.remove('header--no-js');
+
+menuToggle.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  menuToggle.classList.toggle('header__menu-toggle--close');
+  menu.classList.toggle('header__menu--opened');
+});
